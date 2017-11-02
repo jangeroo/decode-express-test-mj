9312cae5679c4ef2a9c388738a48f937c195f3e1
Michael Jang <michaeljang86@gmail.com>
2017-11-02 16:59:58 -0400

    Added /random get

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const app = require('express')()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
-    console.log('Express app listening on port', app.get('port'))
+app.listen(app.get('port'), function () {
+  console.log('Express app listening on port', app.get('port'))
 })
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my new Heroku Express app!')
+  res.send('Welcome to my new Heroku Express app!')
+})
+
+app.get('/random', (req, res) => {
+  let num = Math.floor(Math.random() * 5)
+  switch (num) {
+    case 1:
+      res.send('You got a random "one"');
+      break;
+    case 2:
+      res.send('You got a random "two"');
+      break;
+    case 3:
+      res.send('You got a random "three"');
+      break;
+    default:
+      res.send('You didn\'t get a lucky number')
+  }
 })
